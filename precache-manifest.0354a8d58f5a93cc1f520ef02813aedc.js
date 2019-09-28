9b70f475268adb8e0c5d992078d1541212f743c7
Stanley Lalanne <stanleefit@icloud.com>
2019-09-28 15:09:22 -0400

    Updates

---
+++ precache-manifest.0354a8d58f5a93cc1f520ef02813aedc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4c796df84ae9fc584369b057bd4517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce30f5a4437a1ae5e7a7",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "401b1885d180964cf1e4",
+    "url": "/static/js/2.141909c0.chunk.js"
+  },
+  {
+    "revision": "ce30f5a4437a1ae5e7a7",
+    "url": "/static/js/main.6cb10565.chunk.js"
+  },
+  {
+    "revision": "f0e8c2ef4fec1fcf6785",
+    "url": "/static/js/runtime-main.9db08c93.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
